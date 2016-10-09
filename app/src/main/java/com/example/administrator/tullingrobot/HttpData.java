67abb2de02f4a98f64ccf2b4b7b62cc11b707db0
ms772010 <790327865@qq.com>
2016-10-09 11:13:55 +0800

    HttpData类

---
+++ app/src/main/java/com/example/administrator/tullingrobot/HttpData.java
@@ -0,0 +1,15 @@
+package com.example.administrator.tullingrobot;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by Administrator on 2016/10/9 0009.
+ */
+public class HttpData extends AsyncTask<String,Void,String> {
+
+
+    @Override
+    protected String doInBackground(String... params) {
+        return null;
+    }
+}
