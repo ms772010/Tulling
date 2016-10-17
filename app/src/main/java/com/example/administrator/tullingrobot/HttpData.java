a22430d90519935f0cb4f61bc74b94b3d3ae38aa
ms772010 <790327865@qq.com>
2016-10-17 17:31:04 +0800

    urlconnection

--- app/src/main/java/com/example/administrator/tullingrobot/HttpData.java
+++ app/src/main/java/com/example/administrator/tullingrobot/HttpData.java
@@ -2,31 +2,50 @@ package com.example.administrator.tullingrobot;
 
 import android.os.AsyncTask;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.MalformedURLException;
+
 
 /**
  * Created by Administrator on 2016/10/9 0009.
  */
 public class HttpData extends AsyncTask<String,Void,String> {
     private String requesturl;//requesturl是图灵接口地址
-
+    private URL url=null;
+    private HttpURLConnection urlConnection=null;
     public HttpData(String requesturl){
         this.requesturl=requesturl;
     }
     @Override
     protected String doInBackground(String... params) {
-        URL url= null;
-        HttpURLConnection urlConnection = null;
         try {
-            url = new URL(requesturl);
-            urlConnection = (HttpURLConnection)url.openConnection();//建立链接
+            url =new URL(requesturl);
+            urlConnection=(HttpURLConnection)url.openConnection();
         } catch (MalformedURLException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+        try{
+            urlConnection.setDoOutput(true);
+            urlConnection.setChunkedStreamingMode(0);
+
+            OutputStream out = new BufferedOutputStream(urlConnection.getOutputStream());
+            writeStream(out);
+
+            InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+            readStream(in);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+           urlConnection.disconnect();
         }
 
 
