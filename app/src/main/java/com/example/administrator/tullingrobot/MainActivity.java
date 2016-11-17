a5104b47beef4b48acf16a211fcea13fe8ca0742
ms772010 <790327865@qq.com>
2016-11-17 12:15:04 +0800

    json格式接受和读取

--- app/src/main/java/com/example/administrator/tullingrobot/MainActivity.java
+++ app/src/main/java/com/example/administrator/tullingrobot/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.administrator.tullingrobot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements HttpGetDataListener {
     private HttpData myHttpData;
+    private List<ListData> lists;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,9 +17,13 @@ public class MainActivity extends AppCompatActivity implements HttpGetDataListen
         myHttpData=(HttpData) new HttpData("你好",this).execute();
 
     }
-
+    private void initView(){
+        lists=new ArrayList<ListData>();
+    }
     @Override
     public void getDataUrl(String data) {
         System.out.println(data);
+        ListData listData=new ListData(data);
+        lists.add(listData);
     }
 }
