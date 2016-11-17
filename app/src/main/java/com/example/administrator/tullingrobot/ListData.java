b5e3ef0ae916f2fa5d1b16c7bce39b5f57ad5eb4
ms772010 <790327865@qq.com>
2016-11-17 12:03:04 +0800

    json格式接受和读取

---
+++ app/src/main/java/com/example/administrator/tullingrobot/ListData.java
@@ -0,0 +1,20 @@
+package com.example.administrator.tullingrobot;
+
+/**
+ * Created by Administrator on 2016/11/17 0017.
+ */
+public class ListData {
+    private String content;
+    public ListData(String content){
+        setContent(content);
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+
+    public String getContent() {
+        return content;
+    }
+}
