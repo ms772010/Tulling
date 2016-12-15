c719818fa2376efc36f862e3afd61c7812b0aa0d
ms772010 <790327865@qq.com>
2016-12-15 20:16:10 +0800

    fix3 .demo1

--- app/src/main/java/com/example/administrator/tullingrobot/MainActivity.java
+++ app/src/main/java/com/example/administrator/tullingrobot/MainActivity.java
@@ -25,7 +25,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toast;
+
 
 import java.io.File;
 import java.io.FileNotFoundException;
--- app/src/main/java/com/example/administrator/tullingrobot/TextAdapter.java
+++ app/src/main/java/com/example/administrator/tullingrobot/TextAdapter.java
@@ -1,10 +1,15 @@
 package com.example.administrator.tullingrobot;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -15,7 +20,8 @@ import java.util.List;
  */
 public class TextAdapter extends BaseAdapter {
     private List<ListData> lists;
-
+    private ImageView ivHead;
+    private static String path="/sdcard/myHead/";
     private Context context;
     private RelativeLayout layout;
     public TextAdapter(List<ListData> lists,Context context){
@@ -40,9 +46,18 @@ public class TextAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        Bitmap bt = BitmapFactory.decodeFile(path + "head.jpg");//从Sd中找头像，转换成Bitmap
+
+
         LayoutInflater inflater=LayoutInflater.from(context);
         if(lists.get(position).getFlag()==ListData.RECEIVER){
             layout= (RelativeLayout) inflater.inflate(R.layout.left_item,null);
+            ivHead = (ImageView) layout.findViewById(R.id.iv);
+            if(bt!=null){
+                @SuppressWarnings("deprecation")
+                Drawable drawable = new BitmapDrawable(bt);//转换成drawable
+                ivHead.setImageDrawable(drawable);
+            }
         }
         if(lists.get(position).getFlag()==ListData.SEND){
             layout= (RelativeLayout) inflater.inflate(R.layout.right_item,null);
